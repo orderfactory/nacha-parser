d8aec094532d1727c20ff884994f221e2997602e
Sergey Nosov <sergey.nosov@orderfactory.com>
2021-05-31 00:36:36 -0700

    Add ach tables

---
+++ OrderFactory.Nacha.Database/dbo/Tables/AchEntry.sql
@@ -0,0 +1,21 @@
+﻿CREATE TABLE [dbo].[AchEntry]
+(
+    [Id]                     UNIQUEIDENTIFIER NOT NULL PRIMARY KEY,
+    [RecordType]             TINYINT          NOT NULL DEFAULT 6,
+    [TransactionCode]        TINYINT          NOT NULL,
+    [ReceivingDfiId]         NCHAR(8)         NOT NULL,
+    [CheckDigit]             TINYINT          NOT NULL,
+    [DfiAccountNumber]       NVARCHAR(17)     NOT NULL,
+    [Amount]                 DECIMAL(12, 2)   NOT NULL,
+    [Identification]         NVARCHAR(15)     NOT NULL,
+    [AchOperator]            NCHAR(1)         NULL,
+    [Name]                   NVARCHAR(22)     NOT NULL,
+    [DiscretionaryData]      NVARCHAR(2)      NOT NULL,
+    [AddendaRecordIndicator] TINYINT          NOT NULL,
+    [TraceNumber]            NVARCHAR(15)     NULL,
+    [OperatorRoutingNumber]  NCHAR(8)         NULL,
+    [DateJulian]             SMALLINT         NULL,
+    [SequenceNumber]         SMALLINT         NULL,
+    [AchBatchId]             UNIQUEIDENTIFIER NOT NULL,
+    CONSTRAINT [FK_AchEntry_ToAchBatch] FOREIGN KEY ([AchBatchId]) REFERENCES [dbo].[AchBatch]([Id]) ON DELETE CASCADE
+)
---
+++ OrderFactory.Nacha.Database/dbo/Tables/AchFile.sql
@@ -0,0 +1,24 @@
+﻿CREATE TABLE [dbo].[AchFile]
+(
+    [Id]                       UNIQUEIDENTIFIER NOT NULL PRIMARY KEY,
+    [RecordType]               TINYINT          NOT NULL DEFAULT 1,
+    [PriorityCode]             TINYINT          NOT NULL,
+    [ImmediateDestination]     NCHAR(9)         NOT NULL,
+    [ImmediateOrigin]          NCHAR(9)         NOT NULL,
+    [CreationDateTime]         DATETIME2        NOT NULL,
+    [FileIdModifier]           NCHAR(1)         NOT NULL,
+    [RecordSize]               SMALLINT         NOT NULL DEFAULT 094,
+    [BlockingFactor]           TINYINT          NOT NULL DEFAULT 10,
+    [FormatCode]               TINYINT          NOT NULL DEFAULT 1,
+    [ImmediateDestinationName] NVARCHAR(23)     NOT NULL,
+    [ImmediateOriginName]      NVARCHAR(23)     NOT NULL,
+    [ReferenceCode]            NVARCHAR(8)      NOT NULL,
+    [ControlRecordType]        TINYINT          NOT NULL DEFAULT 9,
+    [BatchCount]               INT              NOT NULL,
+    [BlockCount]               INT              NOT NULL,
+    [EntryAndAddendaCount]     INT              NOT NULL,
+    [EntryHash]                NUMERIC(10, 0)   NOT NULL,
+    [TotalDebitAmount]         DECIMAL(20, 2)   NOT NULL,
+    [TotalCreditAmount]        DECIMAL(20, 2)   NOT NULL,
+    [Reserved]                 NVARCHAR(39)     NOT NULL
+)
